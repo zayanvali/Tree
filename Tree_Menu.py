a1f55c20d7fe3ef6b660d57affc9d6de8c86e5c3
zayanvali <zayan.vali@icloud.com>
2026-01-07 18:41:21 +0000

    Update Tree_Menu.py

--- Tree_Menu.py
+++ Tree_Menu.py
@@ -15,7 +15,7 @@ def Insert(root, k):
     if root == None:
         return tree(k)
     if root.data > k:
-        root.left_node = Insert(root.right_node, k)
+        root.left_node = Insert(root.left_node, k)
     else:
         root.right_node = Insert(root.right_node, k)
     return root
@@ -30,30 +30,27 @@ def delete(root, key):
     if root is None:
         return root
     if key < root.data:
-        root.leftChild = delete(root.left_node, key)
+        root.left_node = delete(root.left_node, key)
     elif key > root.data:
-        root.rightChild = delete(root.right_node, key)
+        root.right_node = delete(root.right_node, key)
     else:
         #Root has only 1 child
         if root.left_node is None:
-            temp = root.right_node
-            root = None
-            return temp
+            return root.right_node
         #Root has only 1 child
         elif root.right_node is None:
-            temp = root.left_node
-            root = None
-            return temp
+            return root.left_node
         #Root has 2 children
         else:
-            temp = InorderSuccesor(root)
+            temp = InorderSuccesor(root.right_node)
             print("Hello", temp.data)
-            t = root.data
             root.data = temp.data
-            temp.data = t
-            root.right_node = delete(root.rightChild, temp.data)
+            root.right_node = delete(root.right_node, temp.data)
+    return root
 
 def search(root, value):
+    if root is None:
+        return False
     if root.data == value:
         return True
     elif root.data > value and root.left_node != None:
@@ -63,27 +60,27 @@ def search(root, value):
     else:
         return False
 root = tree(9)
-root.left_node = tree(5)
-root.left_node.left_node = tree(4)
-root.right_node = tree(20)
-root.right_node.left_node = tree(17)
-root.right_node.right_node = tree(23)
-root = Insert(root, 3)
-inorder_traversal(root)
+for value in [7, 1, 4, 8, 6, 9, 3]:
+    root = Insert(root, value)
 
-number = int(input("Please enter a number from 1-4: "))
+number = int(input("Please enter a number from 1-4:\n 1. inorder_traversal \n 2. Insertion \n 3. Deletion \n 4. Searching \n Enter your answer: "))
 
 if number == 1:
     inorder_traversal(root)
 
 elif number == 2:
-    root = Insert(root, 3)
+    value = int(input("Enter the value to insert: "))
+    root = Insert(root, value)
 
 elif number == 3:
-    root = delete(root, key)
-
+    key = int(input("Enter the value to delete: "))
+    answer = delete(root, key)
+    print(answer)
+    
 elif number == 4:
-    root = search(root, value)
+    value = int(input("Enter the value to search"))
+    answer = search(root, value)
+    print(f"found:{answer}")
 
 else:
     print("Invalid answer")
